90426077946e4f94fd009d20dc95e2c8ede9322f
zoey-on-github <88806187+zoey-on-github@users.noreply.github.com>
2023-09-09 21:16:38 -0700

    added an actual ascii raccoon

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,13 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     if args.iter().any(|i| i=="ascii"){
-    println!("test");
+   println!(r"/\ /\");
+   println!(r"''-   _");
+   println!(r"() ()\  ,'_\");
+   println!(r"( . ) )/._./");
+   println!(r"_)-(_).--'");                                            
     } else {
-        println!("racc")
+        println!("racc");
     } 
     
     
